e93be6736c7491b55875af850a4091fb9268b1d5
Cesar Medina <medinacesar918@gmail.com>
2024-02-19 11:53:53 -0300

    inital version

---
+++ src/main/java/com/example/OktaSSOdemo/OktaSsoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.OktaSSOdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaSsoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaSsoDemoApplication.class, args);
+	}
+
+}
