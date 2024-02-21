7214ba9f53d33b5ec74f85c0df376261a841a53a
Cesar Medina <medinacesar918@gmail.com>
2024-02-21 13:45:05 -0300

    Is added security with Okta, own a Social login with Google and GitHub providers, travels with Swagger to document the API

--- src/main/java/com/example/OktaSSOdemo/OktaSsoDemoApplication.java
+++ src/main/java/com/example/OktaSSOdemo/OktaSsoDemoApplication.java
@@ -1,9 +1,31 @@
 package com.example.OktaSSOdemo;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "okta",
+				scheme = "bearer",
+				bearerFormat = "jwt",
+				type = SecuritySchemeType.OAUTH2,
+				in = SecuritySchemeIn.HEADER,
+				openIdConnectUrl = "https://dev-60369700.okta.com/oauth2/default/v1/keys",
+				flows = @OAuthFlows(
+						clientCredentials = @OAuthFlow(
+								authorizationUrl = "https://dev-60369700.okta.com/oauth2/default/v1/authorize",
+								tokenUrl = "https://dev-60369700.okta.com/oauth2/default/v1/token",
+								refreshUrl = ""
+//								scopes = {@OAuthScope(name = "openid"),
+//										  @OAuthScope(name = "profile"),
+//										  @OAuthScope(name = "email")}
+						)
+				))
 public class OktaSsoDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/OktaSSOdemo/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.OktaSSOdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> {
+                        request.requestMatchers("/").permitAll();
+                        request.requestMatchers("/v3/api-docs/**").permitAll();
+                        request.requestMatchers("/swagger-ui/**").permitAll();
+                        request.anyRequest().authenticated();})
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+}
